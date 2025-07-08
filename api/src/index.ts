ba494392f58f6b5749df229e8e168c926e5e7920
Frankline Onuoha <kwituxy@gmail.com>
2025-07-08 01:28:22 +0100

    Products Routes

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,18 @@
 import express from 'express'; 
+import productsRoutes from './routes/products/index'; 
+
 
 // const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
+
+app.use('/products', productsRoutes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
